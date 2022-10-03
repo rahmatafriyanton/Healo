0134492edd62dff347b1cb525d011409e5956e87
Rahmat Afriyanton <rahmatafriyanton@gmail.com>
2022-10-03 21:49:57 +0700

    Initial Setup

---
+++ Healo/View/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Healo
+//
+//  Created by Rahmat Afriyanton on 03/10/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .red
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Podfile
+use_frameworks!
+
+target 'Healo' do
+    pod 'RxSwift', '6.5.0'
+    pod 'RxCocoa', '6.5.0'
+pod 'SnapKit', '~> 5.0.0'
+pod 'MessageKit', '~> 3.3.0'
+pod 'RxDataSources', '~> 5.0'
+end
