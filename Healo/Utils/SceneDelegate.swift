d1cf5e71156f747932fca652f1629ecff4b14f85
elvinajc <elvina14jc@gmail.com>
2022-11-01 10:47:10 +0700

    change scene del

--- Healo/Utils/SceneDelegate.swift
+++ Healo/Utils/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		guard let scene = (scene as? UIWindowScene) else { return }
 		window = UIWindow(windowScene: scene)
 //        window?.rootViewController = UINavigationController(rootViewController: PairingSuccessVC())
-        window?.rootViewController = UINavigationController(rootViewController: TestExplanationVC())
+        window?.rootViewController = UINavigationController(rootViewController: SeekerTabBarVC())
 		window?.makeKeyAndVisible()
 	}
 	func sceneDidDisconnect(_ scene: UIScene) {
