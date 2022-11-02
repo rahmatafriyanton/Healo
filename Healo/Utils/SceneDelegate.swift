d51440de83a843a2b89cb2fa7345bb18d49a492c
elvinajc <elvina14jc@gmail.com>
2022-11-02 19:16:22 +0700

    countdown logic done

--- Healo/Utils/SceneDelegate.swift
+++ Healo/Utils/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		guard let scene = (scene as? UIWindowScene) else { return }
 		window = UIWindow(windowScene: scene)
 //        window?.rootViewController = UINavigationController(rootViewController: CriteriaVC())
-        window?.rootViewController = UINavigationController(rootViewController: SeekerTabBarVC())
+        window?.rootViewController = UINavigationController(rootViewController: TestExplanationVC())
 		window?.makeKeyAndVisible()
 	}
 	func sceneDidDisconnect(_ scene: UIScene) {
